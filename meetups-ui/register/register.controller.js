b8d2bf5e2722b9ac53b4a2351c713057108f0d53
kaushiksahoo <>
2017-08-26 14:06:28 +0530

    Added registration page

--- meetups-ui/register/register.controller.js
+++ meetups-ui/register/register.controller.js
@@ -5,14 +5,16 @@
         .module('app')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
+    RegisterController.$inject = ['UserService', '$scope', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $scope, $location, $rootScope, FlashService) {
         var vm = this;
 
         vm.register = register;
 
         function register() {
             vm.dataLoading = true;
+            vm.user.location = $scope.location;
+            console.log(vm.user);
             UserService.Create(vm.user)
                 .then(function (response) {
                     if (response.success) {
@@ -23,6 +25,14 @@
                         vm.dataLoading = false;
                     }
                 });
+        }
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                $scope.$apply(function(){
+                    $scope.location = position;
+                });
+            });
         }
     }
 
